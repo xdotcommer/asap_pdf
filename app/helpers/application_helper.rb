c155db800608cf8e8e37b6f4a2d520cb9dfd1e4a
Michael Cowden <michael.cowden@gmail.com>
2025-02-19 13:22:08 -0500

    ui touchups

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,6 +16,10 @@ module ApplicationHelper
     {text: text, url: url}
   end
 
+  def format_metadata(value)
+    value.presence || "—"
+  end
+
   def short_number(number)
     number_to_human(number, format: "%n%u", units: {thousand: "k", million: "M", billion: "B"})
   end

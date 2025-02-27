c848da38b89fb1d56b0c516883060587ae2a1f84
Michael Cowden <michael.cowden@gmail.com>
2025-02-27 11:42:49 -0500

    debugging

--- terraform/modules/deployment/main.tf
+++ terraform/modules/deployment/main.tf
@@ -48,11 +48,11 @@ resource "aws_iam_role" "github_actions" {
         }
         Condition = {
           StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:aud"        = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:repository" = var.github_repository
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub"        = ["repo:${var.github_repository}:*"]
-            "token.actions.githubusercontent.com:repository" = var.github_repository
+            "token.actions.githubusercontent.com:sub" = ["repo:${var.github_repository}:*", "repo:${var.github_repository}:environment:production"]
           }
         }
       }

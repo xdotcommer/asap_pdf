8e2c7b10d4dff54b76bf15c205e3f3a671dd1f3f
Michael Cowden <michael.cowden@gmail.com>
2025-02-27 11:51:26 -0500

    update thumbprints

--- terraform/modules/deployment/main.tf
+++ terraform/modules/deployment/main.tf
@@ -19,13 +19,15 @@ resource "aws_ecr_repository" "app" {
 }
 
 # GitHub OIDC Provider
+# https://github.blog/changelog/2023-06-27-github-actions-update-on-oidc-integration-with-aws/
 resource "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
 
   client_id_list = ["sts.amazonaws.com"]
 
   thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1" # GitHub's OIDC thumbprint
+    "6938fd4d98bab03faadb97b34396831e3780aea1", # GitHub's OIDC thumbprint
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"  # GitHub's OIDC v2 thumbprint
   ]
 
   tags = {

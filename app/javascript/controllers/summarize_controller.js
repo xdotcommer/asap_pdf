8fe6d48eac70658c6b3b11ba187e14e2c3b1e9a1
Leo Kacenjar <lkacenja@gmail.com>
2025-02-27 15:31:03 -0700

    ASAP-19 Add routing and summary front end.

---
+++ app/javascript/controllers/summarize_controller.js
@@ -0,0 +1,35 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+    static targets = ["display", "button", "preloader"]
+
+    static values = {
+        documentId: Number,
+    }
+
+    async getSummary() {
+        try {
+            this.buttonTarget.classList.add('hidden');
+            this.preloaderTarget.classList.remove('hidden')
+            const response = await fetch(`/documents/${this.documentIdValue}/update_summary`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content,
+                    "Accept": "application/json"
+                },
+            })
+            if (response.ok) {
+                const jsonSummary = await response.json()
+                this.displayTarget.textContent = jsonSummary.display_text;
+                this.preloaderTarget.classList.add('hidden')
+            } else {
+                this.displayTarget.textContent = 'An error occurred summarizing this document. Please try again later.';
+                throw new Error("Response was not OK")
+            }
+        } catch (error) {
+            console.error("Error summarizing document:", error)
+            this.preloaderTarget.classList.add('hidden')
+        }
+    }
+}

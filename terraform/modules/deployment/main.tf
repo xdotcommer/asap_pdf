c5b6696ebc95766168d728125c26cc2048090e7c
Michael Cowden <michael.cowden@gmail.com>
2025-02-27 11:37:57 -0500

    debug deploy

--- terraform/modules/deployment/main.tf
+++ terraform/modules/deployment/main.tf
@@ -51,7 +51,7 @@ resource "aws_iam_role" "github_actions" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub"        = ["repo:${var.github_repository}:*", "repo:${var.github_repository}:ref:refs/heads/main"]
+            "token.actions.githubusercontent.com:sub"        = ["repo:${var.github_repository}:*"]
             "token.actions.githubusercontent.com:repository" = var.github_repository
           }
         }
